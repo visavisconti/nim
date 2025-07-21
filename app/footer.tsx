623f1622a6058622f9463dfc9865addf58257f31
Bruno Visconti <65882022+visavisconti@users.noreply.github.com>
2025-07-21 18:55:12 +0200

    footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,10 +70,10 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://github.com/visavisconti/nim" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
-            <span>© 2024 Nim.</span>
-            <span>Built with Motion-Primitives.</span>
+            <span>Nim.</span>
+            <span>@ Github.</span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
