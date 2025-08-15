286632fb07da473d43809fefa1ef84a70a2a7318
Bruno Visconti <65882022+visavisconti@users.noreply.github.com>
2025-08-15 09:37:08 +0200

    logo

--- app/header.tsx
+++ app/header.tsx
@@ -4,22 +4,23 @@ import Link from 'next/link'
 
 export function Header() {
   return (
-    <header className="mb-8 flex items-center justify-between">
+    <header className="mb-6 flex items-center justify-between">
       <div>
-        <Link href="/" className="font-medium text-black dark:text-white">
-          Bilgisaray
+        <Link href="/" className="font-medium text-black dark:text-white inline-block mb-0">
+          <img src="image/logo_bilgisaray.png" alt="Bilgisaray" />
         </Link>
         <TextEffect
           as="p"
           preset="fade"
           per="char"
-          className="text-zinc-600 dark:text-zinc-500"
+          className="text-zinc-600 dark:text-zinc-500 text-lg font-bold" // Schriftgröße größer & fett
           delay={0.5}
+          style={{ marginTop: '0rem' }} // zusätzlicher Abstand oberhalb des Texts
         >
           gelebte Solidarität in der Oranienstr. 45 und darüber hinaus
         </TextEffect>
-
       </div>
     </header>
+
   )
 }
